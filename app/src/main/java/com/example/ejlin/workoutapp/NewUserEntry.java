ac75efa19c4ccbf4fe9e77b88fd1a46813019ddf
Ed Lingo <ejlingo@gmail.com>
2018-03-09 22:15:22 -0800

    Updated

--- app/src/main/java/com/example/ejlin/workoutapp/NewUserEntry.java
+++ app/src/main/java/com/example/ejlin/workoutapp/NewUserEntry.java
@@ -1,7 +1,9 @@
 package com.example.ejlin.workoutapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewUserEntry extends AppCompatActivity {
 
@@ -14,4 +16,9 @@ public class NewUserEntry extends AppCompatActivity {
     }
 
 
+    public void navigateToMenu(View view) {
+
+        Intent toMenu = new Intent(this,WorkoutDiary.class);
+        startActivity(toMenu);
+    }
 }
---
+++ app/src/main/java/com/example/ejlin/workoutapp/WorkoutDiary.java
@@ -0,0 +1,14 @@
+package com.example.ejlin.workoutapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class WorkoutDiary extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_diary);
+    }
+
+}
