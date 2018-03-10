3b95fdaee8c9310cc8fa454942b3f7e7b51fa67b
Ed Lingo <ejlingo@gmail.com>
2018-03-09 20:19:15 -0800

    Initial commit

---
+++ app/src/main/java/com/example/ejlin/workoutapp/NewUserEntry.java
@@ -0,0 +1,17 @@
+package com.example.ejlin.workoutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewUserEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_entry);
+
+
+    }
+
+
+}
